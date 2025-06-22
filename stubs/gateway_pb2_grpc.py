c6775f7d0bd8a9fd96f252663c7d11735ffb1a2d
Usov Nikita <nikitosu27@gmail.com>
2025-06-22 23:35:24 +0700

    34

--- stubs/gateway_pb2_grpc.py
+++ stubs/gateway_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-from . import gateway_pb2 as gateway__pb2
+import gateway_pb2 as gateway__pb2
 
 GRPC_GENERATED_VERSION = '1.72.1'
 GRPC_VERSION = grpc.__version__
--- stubs/service_math_solve_pb2_grpc.py
+++ stubs/service_math_solve_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-from . import service_math_solve_pb2 as service__math__solve__pb2
+import service_math_solve_pb2 as service__math__solve__pb2
 
 GRPC_GENERATED_VERSION = '1.72.1'
 GRPC_VERSION = grpc.__version__
